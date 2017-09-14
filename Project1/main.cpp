aad1d5e359add668b5e48ce1297442dcf8fd0f2d
mettewirak <mettewirak@hotmail.com>
2017-09-14 15:51:37 +0200

    Moved the power

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -49,14 +49,14 @@ double* u_vector =new double[n];
 double a=-1.0;
 double b=2.0;
 double c=-1.0;
-
+double power=pow(h,2);
+    
 for(int i=0; i<=n; i++){
     *(a_vector + i)=a;
     *(b_vector + i)=b;
     *(c_vector + i)=c;
     *(x_vector+i)=h*i;
-    *(tilde_vector + i)=f_analytic(x_vector[i]);
-
+    *(tilde_vector + i)=f_analytic(x_vector[i])*power;
     *(u_vector+i)=u(x_vector[i]);
 }
 
@@ -64,20 +64,19 @@ for(int i=0; i<=n; i++){
 for(int i=1;i<(n);i+=1 ){
     *(b_vector+i+1)=*(b_vector+i+1)-*(c_vector+i)*(*(a_vector+i)/(*(b_vector+i)));
     *(tilde_vector +i)=*(tilde_vector+i)-*(tilde_vector+i-1)*(*(a_vector+i)/(*(b_vector+i)));
-    *(a_vector+i)=*(a_vector+i)-*(b_vector+i)*(*(a_vector+i)/(*(b_vector+i)));
+    *(a_vector+i)=0;
 
 }
 
 //Finner verdien til v_n
 
-*(v_vector +n-1)=*(tilde_vector +n-1)*pow(h,2)/(*(b_vector +n-1));
+*(v_vector +n-1)=*(tilde_vector +n-1)/(*(b_vector +n-1));
 
 
-//backward substetution
+//backward substitution
 
 for(int k=n-2;k>=0;k-=1){
-    *(v_vector +k)=(*(tilde_vector +k)*pow(h,2)-*(c_vector +k +1)*(*(v_vector +k+1)))/(*(b_vector +k));
-
+    *(v_vector +k)=(*(tilde_vector +k)-*(c_vector +k +1)*(*(v_vector +k+1)))/(*(b_vector +k));
 }
 
 //finne error
@@ -122,6 +121,7 @@ clock_t t1,t2;
 
 
 double h= 1.0/(n);
+double power=pow(h,2);
 double* v_vector = new double [n];
 double* b_vector =new double [n+1];
 double* tilde_vector=new double[n];
@@ -135,28 +135,25 @@ double b=2.0;
 *(b_vector)=b;
 *(tilde_vector + 0)=f_analytic(0);
 *(x_vector+0)=0;
+    
 for(int i=1; i<=n; i++){
-
     *(x_vector+i)=h*i;
     *(b_vector+i) = double(i+1)/double(i);
-    *(tilde_vector +i)=f_analytic(x_vector[i])+*(tilde_vector+i-1)/(*(b_vector+i));
+    *(tilde_vector +i)=f_analytic(x_vector[i])*power;
     *(u_vector+i)=u(x_vector[i]);
 }
 
-
-
-//forward substetution
-
+// Forward substitution
+for(int i=1; i<=n; i++){
+    *(tilde_vector+i)=*(tilde_vector+i)+*(tilde_vector+i-1)/(*(b_vector+i));
+}
 
 //Finner verdien til v_n
-
-*(v_vector +n-1)=*(tilde_vector +n-1)*pow(h,2)/(*(b_vector +n-1));
-
+*(v_vector +n-1)=*(tilde_vector +n-1)*power/(*(b_vector +n-1));
 
 //backward substetution
-
 for(int k=n-2;k>=0;k-=1){
-    *(v_vector +k)=(*(tilde_vector +k)*pow(h,2)+(*(v_vector +k+1)))/(*(b_vector +k));
+    *(v_vector +k)=(*(tilde_vector +k)*power+(*(v_vector +k+1)))/(*(b_vector +k));
 
 }
 
